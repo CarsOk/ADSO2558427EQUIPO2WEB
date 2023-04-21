31681b23fa403caa6b7e423df7ddc7e6f1917785
GoJhon <jsantander1219@gmail.com>
2023-04-21 13:39:48 +0000

    se creo inicio de sesion con devise (ya tiene implementado las demas vistas pero no tienen estilo)

--- creditos3a/config/routes.rb
+++ creditos3a/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'paises/index'
+  get 'home/dashboard'
+  devise_for :users
+  root to: "home#dashboard"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- pruebaruby/app/models/apprentice.rb
+++
@@ -1,2 +0,0 @@
-class Apprentice < ApplicationRecord
-end
