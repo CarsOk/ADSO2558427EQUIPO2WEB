d597845a8b3257b35adc05e80253935c379a9885
GoJhon <jsantander1219@gmail.com>
2023-04-28 13:02:09 +0000

    se creo modelo producto y se va implementar carrierwave

--- creditos3a/app/controllers/home_controller.rb
+++ creditos3a/app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def dashboard
   end
   def landing_page
-    @productos = Producto.last(4)
+    # @productos_ultimos = Producto.last(4)
+    # @productos_con_descuento = Producto.where('descuento > 0').last(4)
   end
 end
