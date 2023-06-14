87d587071784d267efd8bbb9ce7289f4da9e0469
GoJhon <jsantander1219@gmail.com>
2023-06-14 14:47:53 +0000

    Se implemento visrta de dashboard

--- creditos3a/app/controllers/productos_controller.rb
+++ creditos3a/app/controllers/productos_controller.rb
@@ -1,40 +1,41 @@
 class ProductosController < ApplicationController
-  
+  layout 'admin', only: [:new]
+
   def index
-  @productos = Producto.all
-end
-
-def show
-  @producto = Producto.find(params[:id])
-end
-
-def new
-  @producto = Producto.new
-end
-
-def create
-  @producto = Producto.new(producto_params)
-  if @producto.save
-    flash[:notice] = "Registro guardado"
-    redirect_to producto_path(@producto)
-  else
-    flas[:alert] = "Problemas"
-    render :new
+    @productos = Producto.all
   end
-end
 
-def edit
-  @producto = Producto.find(params[:id])
-end
-
-def update
-  @producto = Producto.find(params[:id])
-  if @producto.update(producto_params)
-    redirect_to producto_path(@producto)
-  else
-    render :edit
+  def show
+    @producto = Producto.find(params[:id])
   end
-end
+
+  def new
+    @producto = Producto.new
+  end
+
+  def create
+    @producto = Producto.new(producto_params)
+    if @producto.save
+      flash[:notice] = "Registro guardado"
+      redirect_to producto_path(@producto)
+    else
+      flas[:alert] = "Problemas"
+      render :new
+    end
+  end
+
+  def edit
+    @producto = Producto.find(params[:id])
+  end
+
+  def update
+    @producto = Producto.find(params[:id])
+    if @producto.update(producto_params)
+      redirect_to producto_path(@producto)
+    else
+      render :edit
+    end
+  end
 
   def delete
   
