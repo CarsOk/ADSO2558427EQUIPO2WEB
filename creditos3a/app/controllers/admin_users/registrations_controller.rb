486ffcef1e7b7f70d3d2857621dbb25b4a30f11b
Jesusvillanueva07 <110982601+Jesusvillanueva07@users.noreply.github.com>
2023-10-11 17:06:40 -0500

    vista de administradores y correciones

--- creditos3a/app/controllers/admin_users/registrations_controller.rb
+++ creditos3a/app/controllers/admin_users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AdminUsers::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+   before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -54,9 +54,9 @@ class AdminUsers::RegistrationsController < Devise::RegistrationsController
   # def after_sign_up_path_for(resource)
   #   super(resource)
   # end
+   protected
 
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :second_name, :last_name_1, :last_name_2, :identification, :address])
+  end
 end
--- creditos3a/app/controllers/dashboard_controller.rb
+++ creditos3a/app/controllers/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_admin_user!
   layout 'admin'
   def home
+  end
+  def admin_users
+    @admin_users = AdminUser.all
   end
 end
