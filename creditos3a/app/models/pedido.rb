c088b8e53d02fbc3d0540bb81f79b517d697bac5
Jesusvillanueva07 <villanuevadavid04@gmail.com>
2023-10-14 18:41:46 -0500

    listas de pedidos de usuario

--- creditos3a/app/models/pedido.rb
+++ creditos3a/app/models/pedido.rb
@@ -3,4 +3,6 @@ class Pedido < ApplicationRecord
   belongs_to :producto
   validates :user, presence: true
   validates :producto, presence: true
+  #validates :comentarios, presence: true
+  #validates :metodo_pago, presence: true
 end
