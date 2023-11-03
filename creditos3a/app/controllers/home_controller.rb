cbe4af963b6ac0476f42ff46fc1ff35cb6265933
Jesusvillanueva07 <villanuevadavid04@gmail.com>
2023-11-02 23:40:02 -0500

    upadate

--- creditos3a/app/controllers/home_controller.rb
+++ creditos3a/app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
   def landing_page
     @productos_ultimos = Producto.first(20)
     @categorias = Categoria.all
+    @productos = Producto.all.order(rating_average: :desc).limit(5)
+
   end
 
   def contacto
