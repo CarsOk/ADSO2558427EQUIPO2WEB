7fc97693c6e9f9551c8e4d05f361212b8112ab97
Jesusvillanueva07 <villanuevadavid04@gmail.com>
2023-11-30 12:15:58 -0500

    cambios

--- creditos3a/db/schema.rb
+++ creditos3a/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_29_022047) do
+ActiveRecord::Schema.define(version: 2023_11_30_165109) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -139,6 +139,7 @@ ActiveRecord::Schema.define(version: 2023_11_29_022047) do
     t.integer "identification"
     t.boolean "administrador"
     t.string "imagen"
+    t.string "telefono"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
