389221176f63096959a920be260848a2057c3d31
breitner daniel mora quevedo <110871046+Breitner12@users.noreply.github.com>
2023-12-05 13:02:41 -0500

    validaciones

--- creditos3a/app/models/user.rb
+++ creditos3a/app/models/user.rb
@@ -1,31 +1,70 @@
+# app/models/user.rb
+
 class User < ApplicationRecord
   has_many :pedidos
   has_many :calificaciones
   has_many :contactopqrs
 
   before_create :set_default_administrador
-  
-  validates :first_name, presence: true
-  #validates :second_name, presence: true
-  validates :last_name_1, presence: true
-  #validates :last_name_2, presence: true
-  validates :identification, presence: true
-  validates :address, presence: true
-  #validates :telefono, presence: true
+  validates :first_name, :last_name_1, :last_name_2, format: { without: /\d/, message: "no debe contener números" }
+  validates :first_name, :last_name_1, :identification, :address, presence: true
+  validates :identification, numericality: { only_integer: true }
+
+  validate :validar_identificacion_sin_letras
+  validate :validar_telefono_sin_letras
+  validate :validar_first_name_sin_numeros
+  validate :validar_second_name_sin_numeros
+  validate :validar_last_name_2_sin_numeros
+  validate :validar_last_name_1_sin_numeros
+
+  # Otras validaciones y asociaciones ...
+
   has_one_attached :imagen
   
   attribute :administrador, :boolean, default: false
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   private
+ 
+  def validar_last_name_1_sin_numeros
+    if last_name_1.to_s.match?(/\d/)
+      errors.add(:last_name_1, "no debe contener números")
+    end
+  end
 
-
+  def validar_last_name_2_sin_numeros
+    if last_name_2.to_s.match?(/\d/)
+      errors.add(:last_name_2, "no debe contener números")
+    end
+  end
+   
+  def validar_second_name_sin_numeros
+    if second_name.to_s.match?(/\d/)
+      errors.add(:second_name, "no debe contener números")
+    end
+  end
+  def validar_first_name_sin_numeros
+    if first_name.to_s.match?(/\d/)
+      errors.add(:first_name, "no debe contener números")
+    end
+  end
   def set_default_administrador
     self.administrador = (self.password.include? "CrediAdmin")
   end
-  
+
+  def validar_telefono_sin_letras
+    if telefono.to_s.match?(/[a-zA-Z]/)
+      errors.add(:telefono, "no debe contener letras")
+    end
+  end
+
+  def validar_identificacion_sin_letras
+    if identification.to_s.match?(/[a-zA-Z]/)
+      errors.add(:identification, "no debe contener letras")
+    end
+  end
 end
+
+
